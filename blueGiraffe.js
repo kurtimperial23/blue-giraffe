53401fb575e6db600a24a4c61c1955b7ee25fc82
Kurt Imperial <76236273+kurtimperial23@users.noreply.github.com>
2023-05-28 12:28:10 +0800

    Add files via upload

---
+++ blueGiraffe.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerMode: true,
+  prevArrow: $('.prev'),
+  nextArrow: $('.next'),
+  centerPadding: '20%',
+});
+  
